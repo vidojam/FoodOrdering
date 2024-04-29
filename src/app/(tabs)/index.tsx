93d7c3610d4622d19c14e584eae9f0f7f9cd22f9
Jose Torres <vidojam@gmail.com>
2024-04-29 16:04:19 -0400

    Begin work on UI

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,30 +2,30 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/src/components/EditScreenInfo';
 import { Text, View } from '@/src/components/Themed';
+import { startTransition } from 'react';
+import Colors from '../../constants/Colors';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Maria Cecilia, Baby</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Pepperoni Pizza</Text>
+      <Text style={styles.price} >$12.99</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 18,
+    fontWeight: '700',
+    marginVertical: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
   },
 });
