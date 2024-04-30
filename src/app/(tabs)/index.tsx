9f1d31c6a4122cb2c7f327454d5da1d3bfad673d
Jose Torres <vidojam@gmail.com>
2024-04-30 14:31:49 -0400

    ProductListItems to UI

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,15 +2,25 @@ import { StyleSheet,Text, View, Image } from 'react-native';
 import Colors from '../../constants/Colors';
 import products from '../../../assets/data/products';
 
-const product = products[1];
 
-export default function TabOneScreen() {
-  return (
+
+const ProductListItem = ({ product }) => {
+
+  return ( 
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+    <Image source={{ uri: product.image }} style={styles.image} />
 
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price} >${product.price}</Text>
+    <Text style={styles.title}>{product.name}</Text>
+    <Text style={styles.price} >${product.price}</Text>
+  </View>
+  )
+}   
+
+export default function MenuScreen() {
+  return (
+    <View>
+      <ProductListItem product={products[0]}/>
+      <ProductListItem product={products[1]}/>
     </View>
   );
 }
