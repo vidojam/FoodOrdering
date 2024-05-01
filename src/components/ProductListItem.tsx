8a8892085223b5219e3be89f160221d27176cb7e
Jose Torres <vidojam@gmail.com>
2024-05-01 11:50:35 -0400

    prop types error fix

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,46 @@
+import { StyleSheet,Text, View, Image } from 'react-native';
+import Colors from '../constants/Colors';
+import { Product } from '../types';
+
+export const defaultPizzaImage=
+'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png' ;
+
+type ProductListItemProps = { 
+  product: Product;
+}
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+
+  return ( 
+    <View style={styles.container}>
+    <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+
+    <Text style={styles.title}>{product.name}</Text>
+    <Text style={styles.price} >${product.price}</Text>
+  </View>
+  )
+}   
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+  },
+
+  image: { 
+    width:'100%',
+    aspectRatio: 1,
+      
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '700',
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  },
+});
