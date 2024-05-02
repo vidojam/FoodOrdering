0ab097ec072e9c573ecaa736a38afb30d0d7ea46
Jose Torres <vidojam@gmail.com>
2024-05-02 13:04:28 -0400

    FlatList setup

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet,Text, View, Image } from 'react-native';
+import { StyleSheet,Text, View, Image, FlatList } from 'react-native';
 import Colors from '../../constants/Colors';
 import products from '../../../assets/data/products';
 import ProductListItem from '@components/ProductListItem'
@@ -8,33 +8,19 @@ import ProductListItem from '@components/ProductListItem'
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/>
+      {/* <ProductListItem product={products[0]}/>
+      <ProductListItem product={products[1]}/> */}
+      <FlatList 
+      data={products} renderItem={({ item}) => <ProductListItem product={item} /> }
+      numColumns={2}
+      contentContainerStyle = {{ gap: 10, padding: 10}}
+      columnWrapperStyle = {{ gap: 10 }}  
+      
+      />
+    
+  
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-
-  image: { 
-    width:'100%',
-    aspectRatio: 1,
-      
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '700',
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-  },
-});
 
 
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -13,7 +13,10 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
 
   return ( 
     <View style={styles.container}>
-    <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+    <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} 
+    resizeMode='contain'
+    /> 
+
 
     <Text style={styles.title}>{product.name}</Text>
     <Text style={styles.price} >${product.price}</Text>
@@ -27,6 +30,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+    // margin: 10,  or contentContainerStyle = {{ gap 10}} in FlatList & columnWrapperStyle = {{gap=10}}
+    flex: 1,
+    maxWidth: '50%', // last item in FlatList will take up the whole width
   },
 
   image: { 
