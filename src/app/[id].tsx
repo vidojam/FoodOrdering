56d6f26c6d8d517803667b392636fe33f370e6d5
Jose Torres <vidojam@gmail.com>
2024-05-03 12:58:53 -0400

    view to pressable

---
+++ src/app/[id].tsx
@@ -0,0 +1,12 @@
+import {useLocalSearchParams} from 'expo-router'
+import {View, Text} from 'react-native';
+
+const ProductDetailsScreen = () => { 
+  const { id } = useLocalSearchParams();
+  return (
+    <View>
+      <Text style={{ fontSize: 20  }}>ProductDetailsScreen for {id}</Text>
+    </View>
+  )
+}
+export default ProductDetailsScreen;i
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet,Text, View, Image } from 'react-native';
+import { StyleSheet,Text, View, Image, Pressable  } from 'react-native';
 import Colors from '../constants/Colors';
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
 export const defaultPizzaImage=
 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png' ;
@@ -12,17 +13,20 @@ type ProductListItemProps = {
 const ProductListItem = ({ product }: ProductListItemProps) => {
 
   return ( 
-    <View style={styles.container}>
-    <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} 
+    <Link href={'/product'} asChild>
+    <Pressable style={styles.container}>
+    <Image 
+    source={{ uri: product.image || defaultPizzaImage }} style={styles.image} 
     resizeMode='contain'
     /> 
 
-
     <Text style={styles.title}>{product.name}</Text>
     <Text style={styles.price} >${product.price}</Text>
-  </View>
-  )
-}   
+
+  </Pressable>
+  </Link>
+  );
+};   
 export default ProductListItem;
 
 const styles = StyleSheet.create({
