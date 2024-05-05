c7c1ff71e194344357505eb8492023f0f93f8052
Jose Torres <vidojam@gmail.com>
2024-05-05 17:35:20 -0400

    sizes coding continues

--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -1,7 +1,9 @@
 import {Stack,useLocalSearchParams} from 'expo-router'
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image,StyleSheet } from 'react-native';
 import products from '@assets/data/products';
 import { defaultPizzaImage } from '@/components/ProductListItem';
+
+const sizes = ['S', 'M', 'L', 'XL'];
 
 const ProductDetailsScreen = () => { 
   const { id } = useLocalSearchParams();
@@ -17,22 +19,54 @@ const ProductDetailsScreen = () => {
       <Stack.Screen options={{ title: product.name }} />
       <Image 
        source={{ uri: product.image || defaultPizzaImage }} 
-       style={styles.image} />
-
+       style={styles.image} 
+       />
+      
+      <Text>Select Size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+        <View style={styles.size} key={size}>
+        <Text style={styles.sizeText}>{size}</Text>
+         </View> 
+        ))}
+      </View>
       <Text style={styles.price }>${product.price}</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+    padding: 10,
+  },
     image: {
       width: '100%',
-      aspectRatio: 1
+      aspectRatio: 1,
     },
-    price: {}
-  },
+    price: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    sizes: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      marginVertical: 10,
+    },
+    size: {
+      backgroundColor: 'gainsboro',
+      width: 50,
+      aspectRatio: 1,
+      borderRadius: 25,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    sizeText: {
+      fontSize: 20,
+      fontWeight: 500,
+    },
+
 });
+
 export default ProductDetailsScreen;
