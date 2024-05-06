1d460ce05c85814fc9ebab1c7e8c81b1e736ece1
Jose Torres <vidojam@gmail.com>
2024-05-06 17:08:53 -0400

    select size & Preesable work

--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -1,17 +1,17 @@
-import {Stack,useLocalSearchParams} from 'expo-router'
-import {View, Text, Image, StyleSheet } from 'react-native';
-import products from '@assets/data/products';
-import { defaultPizzaImage } from '@/components/ProductListItem';
-import { useState } from 'react';
+import { Stack, useLocalSearchParams } from "expo-router";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import { useState } from "react";
 
-const sizes = ['S', 'M', 'L', 'XL'];
+const sizes = ["S", "M", "L", "XL"];
 
-const [selectedSize, setSelectedSize] = useState('M');
+const [selectedSize, setSelectedSize] = useState("M");
 
-const ProductDetailsScreen = () => { 
+const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
-  const product = products.find((p) => p.id.toString() === id); 
+  const product = products.find((p) => p.id.toString() === id);
 
   if (!product) {
     return <Text>Product not found</Text>;
@@ -21,21 +21,39 @@ const ProductDetailsScreen = () => {
     <View style={styles.container}>
       <Stack.Screen options={{ title: product.name }} />
 
-      <Image 
-       source={{ uri: product.image || defaultPizzaImage }} 
-       style={styles.image} 
-       />
-      
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+
       <Text>Select Size</Text>
       <View style={styles.sizes}>
         {sizes.map((size) => (
-        <View style={[styles.size, { backgroundColor: selectedSize === size? 'gainsboro' : 'white'}]} key={size}>
-           <Text style={styles.sizeText}>{size}</Text>
-        </View> 
+          <Pressable
+            onPress={() => {
+              setSelectedSize(size);
+            }}
+            style={[
+              styles.size,
+              {
+                backgroundColor: selectedSize === size ? "gainsboro" : "white",
+              },
+            ]}
+            key={size}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                { color: selectedSize === size ? "black" : "gray" },
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
         ))}
       </View>
 
-      <Text style={styles.price }>${product.price}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
@@ -43,35 +61,34 @@ const ProductDetailsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 10,
   },
-    image: {
-      width: '100%',
-      aspectRatio: 1,
-    },
-    price: {
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    sizes: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      marginVertical: 10,
-    },
-    size: {
-      backgroundColor: 'gainsboro',
-      width: 50,
-      aspectRatio: 1,
-      borderRadius: 25,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    sizeText: {
-      fontSize: 20,
-      fontWeight: '500',
-    },
-
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
 });
 
 export default ProductDetailsScreen;
