1ba12222c9cde01cc8861617ae66ded3d7526539
Jose Torres <vidojam@gmail.com>
2024-05-10 14:43:52 -0400

    expo router check

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,33 +1,29 @@
-import { StyleSheet,Text, View, Image, Pressable  } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import Colors from '../constants/Colors';
 import { Product } from '../types';
 import { Link } from 'expo-router';
 
-export const defaultPizzaImage=
-'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png' ;
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
 type ProductListItemProps = { 
   product: Product;
 }
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
-
   return ( 
     <Link href={`/menu/${product.id}`} asChild>
-    <Pressable style={styles.container}>
-    <Image 
-    source={{ uri: product.image || defaultPizzaImage }} style={styles.image} 
-    resizeMode='contain'
-    /> 
-
-    <Text style={styles.title}>{product.name}</Text>
-    <Text style={styles.price} >${product.price}</Text>
-
-  </Pressable>
-  </Link>
+      <Pressable style={styles.container}>
+        <Image 
+          source={{ uri: product.image || defaultPizzaImage }} 
+          style={styles.image} 
+          resizeMode='contain'
+        /> 
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
   );
-};   
-export default ProductListItem;
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -38,11 +34,9 @@ const styles = StyleSheet.create({
     flex: 1,
     maxWidth: '50%', // last item in FlatList will take up the whole width
   },
-
   image: { 
-    width:'100%',
+    width: '100%',
     aspectRatio: 1,
-      
   },
   title: {
     fontSize: 18,
@@ -54,3 +48,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
+
+export default ProductListItem;
