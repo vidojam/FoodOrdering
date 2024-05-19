1ec5621151efec610264da01f6e0d5dc029c70cc
Jose Torres <vidojam@gmail.com>
2024-05-19 15:51:24 -0400

    Product Details Screen work

---
+++ src/app/(tabs)/menu/ProductDetailsScreen.tsx
@@ -0,0 +1,15 @@
+import { Stack, useLocalSearchParams } from "expo-router";
+import { View, Text } from "react-native";
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View>
+      <Stack.Screen options={{ title: `Details` + id }} />
+      <Text style={{ fontSize: 20 }}>Product Details Screen for id: {id}</Text>
+    </View>
+  );
+};
+
+export default ProductDetailsScreen;
