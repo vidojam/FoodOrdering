4bf52e8a9c52cff1e10cc7d140cc0e8eb93066b4
Jose Torres <vidojam@gmail.com>
2024-05-20 13:49:47 -0400

    products.find

--- src/app/(tabs)/menu/ProductDetailsScreen.tsx
+++ src/app/(tabs)/menu/ProductDetailsScreen.tsx
@@ -1,12 +1,17 @@
 import { Stack, useLocalSearchParams } from "expo-router";
 import { View, Text } from "react-native";
+import products from "@assets/data/products";
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
+  const product = products.find((p) => p.id.toString() === id);
+
   return (
     <View>
-      <Stack.Screen options={{ title: `Details` + id }} />
+      {/* <Stack.Screen options={{ title: `Details` + id }} /> */}
+      <Stack.Screen options={{ title: product?.name }} />
+
       <Text style={{ fontSize: 20 }}>Product Details Screen for id: {id}</Text>
     </View>
   );
